2718feecbf82c72767119452070500ba5fe12975
GuilhermePPCompassUol <guilherme.petry.pb@compasso.com.br>
2022-10-25 15:09:27 -0300

    Criação da classe Anime

---
+++ my_anilist/lib/model/anime.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Anime extends ChangeNotifier {
+  final int id;
+  final String nativeTitle;
+  final String englishTitle;
+  final String image;
+  final String description;
+
+  Anime(this.id, this.nativeTitle, this.englishTitle, this.image, this.description);
+
+  
+}
